c7c8d47ada6a0969ecae30ec370140b44e88f44b
lwooden <lowell.wooden@gmail.com>
2019-05-09 20:28:49 -0400

    created model for users and task w/ sanitization and custom validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { 
@@ -10,19 +11,59 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', { 
      name: { // Properties of the "User" object/class
-         type: String // constructor functions in javascript
+        
+        // constructor functions in javascript
+        // Attributes or characteristics of the properties associated with this object
+        // Data Sanitization
+         type: String, 
+         required: true,
+         trim: true
+     },
+     email: {
+         type: String,
+         required: true,
+         lowercase: true,
+         trim: true,
+
+         // Data Validation
+         validate(value) {
+             if (!validator.isEmail(value)) {
+                 throw new Error("Please insert a valid email address!")
+             }
+         }
      },
      age: {
-         type: Number
-     }
-  })
+         type: Number,
+         default: 0,
+         validate(value) {
+             if (value < 0) {
+                 throw new Error('Age must be a positive number!')
+             }
+         }
+     },
+     password: {
+         type: String,
+         required: true,
+         trim: true,
+         minlength: 7,
+         validate(value) {
+             if (value.toLowerCase().includes('password')) {
+                 throw new Error('Password must not contain the word "password"!')
+            }
+         }
+      }
+ })
 
 const Task = mongoose.model('Task', {
     taskDescr: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+        
     },
     isCompleted: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
@@ -31,13 +72,14 @@ const Task = mongoose.model('Task', {
 // Create an Instance of the Model
 
 // const me = new User({
-//     name: "John",
-//     age: 'Low'
+//     name: "   Low    ",
+//     email: "LOW@low.com",
+//     password: "low"
 // })
 
 const newTask = new Task({
-    taskDescr: "Take out the trash",
-    isCompleted: false
+    //taskDescr: "Mow the lawn",
+    
 })
 
 // Save the Instance of the Model to the database
@@ -45,7 +87,7 @@ const newTask = new Task({
 // me.save().then((me) => {
 //     console.log(me)
 // }).catch((error) => {
-//     console.log(error._message)
+//     console.log(error.message)
 // })
 
 newTask.save().then(() => {
