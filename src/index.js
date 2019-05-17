c80ed889f1ba31f82bed5b82b504d407ba6d6850
lwooden <lowell.wooden@gmail.com>
2019-05-17 14:21:56 -0400

    added more comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const userRouter = require('./routes/userRoutes')
-const taskRouter = require('./routes/taskRoutes')
+const userRouter = require('./routes/userRoutes') // import userRoutes file
+const taskRouter = require('./routes/taskRoutes') // import taskRoutes file
 
 require('./db/mongoose') // ensures that the file (mongodb.js) runs and connects to the database
 
@@ -9,8 +9,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.json()) // ensures express parses all json that is passed to the application
 
-app.use(userRouter)
-app.use(taskRouter)
+app.use(userRouter) // enable user routes
+app.use(taskRouter) // enable task routes
 
 
 app.listen(port, () => {
