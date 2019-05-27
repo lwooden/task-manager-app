f5ca52444cc8b5833c24bbc4f34db24278bc4b32
lwooden <lowell.wooden@gmail.com>
2019-05-27 12:16:31 -0400

    clean up

--- src/index.js
+++ src/index.js
@@ -8,13 +8,45 @@ require('./db/mongoose') // ensures that the file (mongodb.js) runs and connects
 const app = express()
 const port = process.env.PORT || 3000
 
+// Without Middleware:  new request -> execute route handler
+//
+// With Middleware:     new request -> do something -> execute route handler
+
+// <------  Express Middleware Examples ------> 
+// app.use((req, res, next) => {
+//     // console.log(req.method, req.path)
+//     // next() // tells the program to move on to the next piece of logic (executing the route)
+
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     // temporarily disables all routes and sends msg to user
+//     res.status(503).send('Site currently under construction') 
+// })
+// <------  Express Middleware Examples ------> 
+
 app.use(express.json()) // ensures express parses all json that is passed to the application
 app.use(userRouter) // enable user routes
 app.use(taskRouter) // enable task routes
 
-var token = jwt.sign("I love the Lord", "1234" )
-console.log("Your token:", token)
+// Testing Jsonwebtoken
+// const myFunction2 = async () => {
 
+//     var token = jwt.sign("I love the Lord", "1234" )
+//     console.log("Your token:", token)
+
+//     var data = jwt.verify(token, "1234")
+//     console.log(data)
+// }
+
+// myFunction2()
+
+// Testing Bcrypt
 // const myPassword = "Password1"
 // const anotherPassword = "Password2"
 // const saltRounds = 10
