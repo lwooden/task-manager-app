ebb6d40e135746d813eea82d81323ada658bd9c7
lwooden <lowell.wooden@gmail.com>
2019-06-24 16:48:11 -0400

    refactored into a schema like users and added timestamps

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task', {
+// refactor Task model and it's properties into a schema so we can take advantage of middleware
+const taskSchema = mongoose.Schema({
+
+    // Properties of the "Task" object/class
     taskDescr: {
         type: String,
         required: true,
@@ -15,6 +18,32 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User' // create a relationship in the database between the user and task objects
     }
+}, {
+    timestamps: true
+
 })
 
+const Task = mongoose.model('Task', taskSchema)
+
 module.exports = Task
+
+
+// const Task = mongoose.model('Task', {
+//     taskDescr: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     isCompleted: {
+//         type: Boolean,
+//         default: false
+//     },
+//     owner: { // creating a relationship between users and tasks for security
+//         type: mongoose.Schema.Types.ObjectId, // store the ID of the user who created the task
+//         required: true,
+//         ref: 'User' // create a relationship in the database between the user and task objects
+//     }
+// }, {
+//     timestamps: true
+// })
+
