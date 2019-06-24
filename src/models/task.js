58a4c936e6a8db55c5efb91ac660054f1580cc33
lwooden <lowell.wooden@gmail.com>
2019-06-23 23:13:08 -0400

    added owner to task model; added relationship rer

--- src/models/task.js
+++ src/models/task.js
@@ -9,6 +9,11 @@ const Task = mongoose.model('Task', {
     isCompleted: {
         type: Boolean,
         default: false
+    },
+    owner: { // creating a relationship between users and tasks for security
+        type: mongoose.Schema.Types.ObjectId, // store the ID of the user who created the task
+        required: true,
+        ref: 'User' // create a relationship in the database between the user and task objects
     }
 })
 
