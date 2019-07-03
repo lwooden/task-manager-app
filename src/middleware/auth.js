b3fde8e302795fd450766fc3c011150ba1192c62
lwooden <lowell.wooden@gmail.com>
2019-07-03 17:29:14 -0400

    replaced JWT Secret with env variable syntax

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,7 +8,7 @@ const auth = async (req, res, next) => {
        const token = req.header('Authorization').replace('Bearer ', '') 
        // console.log(token)
 
-       const decoded = jwt.verify(token, '1234')
+       const decoded = jwt.verify(token, process.env.JWT_SECRET)
        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token})
        
        if(!user) {
