062b60907c1ba8df91bffa234ec8cdb27bb00c70
lwooden <lowell.wooden@gmail.com>
2019-07-03 17:32:28 -0400

    small edit

--- src/emails/account.js
+++ src/emails/account.js
@@ -2,7 +2,7 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-// The goal for this file is to:
+// The goal for this file is too:
 // 1. Define functions that can be executed when the user does something (sign up, deletes account, etc)
 // 2. Export each function in this file so we can call the functions in the routes file
 // 3. Isolate the code for handling email in it's own file for better structure and organization
