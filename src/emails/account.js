019cc1066b12f0017cacadd447c384eababaeaad
lwooden <lowell.wooden@gmail.com>
2019-07-03 15:28:19 -0400

    created to hold email config and functions

---
+++ src/emails/account.js
@@ -0,0 +1,41 @@
+const sgMail = require('@sendgrid/mail')
+const sendgridAPIKey = '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'
+sgMail.setApiKey(sendgridAPIKey)
+
+// The goal for this file is to:
+// 1. Define functions that can be executed when the user does something (sign up, deletes account, etc)
+// 2. Export each function in this file so we can call the functions in the routes file
+// 3. Isolate the code for handling email in it's own file for better structure and organization
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'lowell.wooden@gmail.com',
+        subject: 'Welcome to Task Manager!',
+        text: `Welcome to the app, ${name}! Let us know how we can assist you!`
+    })
+}
+
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'lowell.wooden@gmail.com',
+        subject: 'Sorry to See You Go :(',
+        text: `Hey ${name}! We noticed that you choose to delete your account. We are terribly sorry to see you go. Is there anything we can do to improve so that this doesn't happen again?`
+
+    })
+}
+
+
+// sgMail.send({
+//     to: 'lowell.wooden@gmail.com',
+//     from: 'lowell.wooden@gmail.com',
+//     subject: 'My First Dev Email',
+//     text: 'Hope this makes it to you!'
+// })
+
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail
+}
