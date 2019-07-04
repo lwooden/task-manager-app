56eccfeab7a3c82f133a09a1d7a8771eaae5d47f
lwooden <lowell.wooden@gmail.com>
2019-07-04 09:35:38 -0400

    added promise chaining and async/await examples

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,11 +1,11 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math')
 
-// Test 1
+
 test('Hello World!', () => {
 
 })
 
-// Test 2
+
 test('Should Calcualte Total w/ Custom Tip', () => {
     const total = calculateTip(10, .3)
 
@@ -18,24 +18,42 @@ test('Should Calcualte Total w/ Custom Tip', () => {
      } */
 })
 
-// Test 3
+
 test('Should Calcualte Total w/ Default Tip', () => {
     const total = calculateTip(10)
     expect(total).toBe(12.5)
 })
 
-// Test 4 
+
 test(' Should Convert Farenheit to Celsius', () => {
     const degreesCelsius = fahrenheitToCelsius(32)
     expect(degreesCelsius).toBe(0)
 })
 
-// Test 5
+
 test('Should Convert Celsius to Farenheit', () => {
     const degreesFarenheit = celsiusToFahrenheit(0)
     expect(degreesFarenheit).toBe(32)
 })
 
+// test('Async Demo', (done) => { // tell jest this is a asynchronous op by adding "done"
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done() // call done() so jest knows that all work has been completed
+//     }, 2000)
+// })
+
+test('Should Add 2 Numbers (Promise Chaining)', (done) => {
+    add(2,3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Should Add 2 Numbers (Async/Await)', async () => {
+    const sum = await add(4,6)
+    expect(sum).toBe(11)
+})
 
 /* Jest comes with an asserstion library */
 
